291afa409f85d35fcdb0fdb4e9396a8af78d2613
renata <renata.olive19@gmail.com>
2021-12-09 10:58:29 +0100

    fix problems in tests.yml

--- github_api_repo.py
+++ github_api_repo.py
@@ -7,7 +7,7 @@ import os
 class GithubSearchTests(unittest.TestCase):
 
     def setUp(self):
-        token = os.environ.get('GH_TOKEN')
+        token = os.environ.get('github-token')
         self.headers = {'Authorization': f'token {token}'}
 
     def make_request(self, params, auth=True):
